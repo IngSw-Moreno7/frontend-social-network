6afc8342162d2d2e6c2e9ff8797fa61cde1b4421
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-07-25 23:49:58 -0500

    Imports style sheets in main.jsx Ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// Importar assets "RECURSO"(hojas de estilo, imágens, fuentes, etc.)
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
+import './assets/css/normalize.css';
+import './assets/css/styles.css';
+import './assets/css/responsive.css';
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+
+  <App />
+
+);
